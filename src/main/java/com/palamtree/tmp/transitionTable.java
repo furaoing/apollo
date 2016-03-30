10edbecbe0f06807941d32a5ef66b9681c7ea0be
furaoing <furaoing@hotmail.com>
2016-03-30 18:10:26 +0800

    initial commit

---
+++ src/main/java/com/palamtree/tmp/transitionTable.java
@@ -0,0 +1,36 @@
+/**
+ * Created by roy on 2016/3/30.
+ */
+package com.palamtree.tmp;
+
+public class transitionTable {
+    public enum State {
+        height;
+
+    }
+
+        /*
+        MyOK(true),
+    Initial( false ),
+    Final( true ),
+    Error( false );
+
+    static public final Integer length = 1 + Error.ordinal();
+
+    final boolean accepting;
+
+    State( boolean accepting ) {
+        this.accepting = accepting;
+    }
+
+}
+    State transition[][] = {
+    //  A               B               C
+    {
+        State.Initial,  State.Final,    State.Error
+    }, {
+        State.Final,    State.Initial,  State.Error
+    }
+};
+*/
+}
---
+++ src/test/java/com/test/transitionTableTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import com.palamtree.tmp.transitionTable.State;
+
+/**
+ * Created by roy on 2016/3/30.
+ */
+public class transitionTableTest {
+    public static void main(String[] args) {
+        State s;
+        s = State.height;
+
+    }
+}
