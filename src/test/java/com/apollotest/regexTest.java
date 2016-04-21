814f4c1c3fde5b4867dc4e555250764bd55e0277
furaoing <furaoing@hotmail.com>
2016-04-21 19:28:25 +0800

    Added a regexEx class to provide findall method for regex matching

--- src/test/java/com/apollotest/regexTest.java
+++ src/test/java/com/apollotest/regexTest.java
@@ -3,29 +3,25 @@ package com.apollotest;
 /**
  * Created by roy on 2016/4/15.
  */
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import com.apollo.common.utils.regexEx;
 
 public class RegexTest
 {
     public static void main( String args[] ){
 
       // String to be scanned to find the pattern.
-      String line = "This order was placed for QT3000! OK?";
-      String pattern = ".+?(o.+?r).+";
+      String line = "我的1999年10月21日之旅我的1998年11月22日之旅";
+      String pattern = "(\\d{4})年(\\d{1,2})月(\\d{1,2})日.*?之旅";
 
       // Create a Pattern object
       Pattern r = Pattern.compile(pattern);
 
-
       // Now create matcher object.
       Matcher m = r.matcher(line);
-
-      if (m.find( )) {
-         System.out.println("Found value: " + m.group(0) );
-          System.out.println("Found value: " + m.group(1) );
-      } else {
-         System.out.println("NO MATCH");
-      }
+      List<List<String>> matches = regexEx.findall(m);
+        int a = 1;
    }
 }
