6e8d52f3023e6a30c252d451a97b7bc69a2c064e
furaoing <furaoing@hotmail.com>
2016-08-10 11:15:48 +0800

    updates

--- src/main/java/net/bigboo/apollo/common/io/FileIO.java
+++ src/main/java/net/bigboo/apollo/common/io/FileIO.java
@@ -9,6 +9,28 @@ public class FileIO {
 
     public static String f_read(String file) {
         try {
+            // TODO: Rewrite f_read function
+            // link: http://stackoverflow.com/questions/4716503/reading-a-plain-text-file-in-java
+            /*
+            public static String readFile(String filename) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String everything;
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        try {
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            everything = sb.toString();
+        } finally {
+            br.close();
+        }
+        return everything;
+    }
+             */
             File fileDir = new File(file);
 
             BufferedReader br = new BufferedReader(
